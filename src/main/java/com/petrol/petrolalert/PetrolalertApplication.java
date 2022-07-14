b7551472c85b4617c0427e1171464faad7051a17
Ryszard Bancarzewski <ryszard.bancarzewski@jamf.com>
2022-07-14 11:13:34 +0200

    Add Kubernetes deployments

---
+++ src/main/java/com/petrol/petrolalert/PetrolalertApplication.java
@@ -0,0 +1,13 @@
+package com.petrol.petrolalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetrolalertApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PetrolalertApplication.class, args);
+  }
+
+}
