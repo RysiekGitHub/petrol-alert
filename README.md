1a69e3c082c452de72cc25f5f6bbc6d522ddf3fa
Ryszard Bancarzewski <ryszard.bancarzewski@jamf.com>
2022-07-27 15:25:44 +0200

    Rest api for stations

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # petrol-alert
+
+Mysql Deployment: (run from src/kubernetes)
+- kubectl apply -f mysql-secret.yaml
+- kubectl apply -f mysql-configmap.yaml
+- kubectl apply -f mysql-credentials.yaml
+- kubectl apply -f mysql-storage.yaml
+- kubectl apply -f mysql-deployment.yaml
+
+Application deployment:
+From project main catalog:
+- ./gradlew assemble
+- ./gradlew docker
+- check that image is inside docker: docker images
+Run from src/kubernetes:
+- kubectl apply -f deployment.yaml
+- check that pods is working: kubectl get pods
+
+Make application work properly:
+- kubectl exec --stdin --tty [mysql-pod-name] -- /bin/bash
+- mysql -u test -p
+-- enter "test" password
+- enter: "use petrolalert"  
+- create table petrol_station(name VARCHAR(255));
+- application should be ready to work properly
+
+Application:
+- http://localhost:32764/stations - get method - return list stations
+- http://localhost:32764/stations - post method - add record with station name 
--- src/main/java/com/petrol/petrolalert/PetrolAlertController.java
+++ src/main/java/com/petrol/petrolalert/PetrolAlertController.java
@@ -1,11 +1,15 @@
 package com.petrol.petrolalert;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class PetrolAlertController {
 
   private final JdbcTemplate jdbcTemplate;
@@ -15,8 +19,15 @@ public class PetrolAlertController {
     this.jdbcTemplate = jdbcTemplate;
   }
 
-  @GetMapping("/create")
-  public void xyz() {
-    jdbcTemplate.execute("CREATE TABLE customers(first_name VARCHAR(255))");
+  @PostMapping("/stations")
+  public ResponseEntity<String> addStation(@RequestBody String name) {
+    jdbcTemplate.update("INSERT INTO petrol_station(name) values(?)", name);
+    return ResponseEntity.accepted().build();
+  }
+
+  @GetMapping("/stations")
+  public ResponseEntity<List<String>> getAllStations() {
+    final List<String> stations = jdbcTemplate.query("SELECT name from petrol_station", (rs, rowNum) -> rs.getString(1));
+    return ResponseEntity.ok(stations);
   }
 }
